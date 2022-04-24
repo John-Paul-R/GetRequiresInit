1726755db803a7b9c1a1f8e87206f346d94373f4
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2022-04-24 00:51:22 -0400

    I confuse myself. Revert Weaver changes for ExAssembly.

--- Bench/Bench.cs
+++ Bench/Bench.cs
@@ -1,56 +1,71 @@
-﻿namespace Bench;
+﻿#nullable  disable
+namespace Bench;
 
-using AssemblyToProcess;
 using BenchmarkDotNet.Attributes;
-using Fody;
 using GetRequiresInit;
 
-[MemoryDiagnoser]
+[SimpleJob()]
 public class Bench
 {
     private PropertyOwner Unchecked = null!;
-    private Class1 ManuallyChecked = null!;
+    private ManuallyChecked ManuallyChecked = null!;
     private CheckedPropertyOwner Checked = null!;
 
     [GlobalSetup]
     public void Setup()
     {
         Unchecked = new PropertyOwner();
-        Unchecked.OwnedProperty = 5;
+        Unchecked.OwnedProperty = "test1";
         
         ManuallyChecked = new();
-        ManuallyChecked.CheckedIntField = 5;
+        ManuallyChecked.CheckedStringField = "test2";
         
         Checked = new();
-        Checked.OwnedProperty = 5;
-        int num = Unchecked.OwnedProperty;
-        num = ManuallyChecked.CheckedIntField;
+        Checked.OwnedProperty = "test3";
+        string num = Unchecked.OwnedProperty;
+        num = ManuallyChecked.CheckedStringField;
         num = Checked.OwnedProperty;
     }
 
     [Benchmark]
-    public int GetIntUnchecked() => Unchecked.OwnedProperty;
+    public string GetIntUnchecked() => Unchecked.OwnedProperty;
 
     [Benchmark]
-    public int GetIntManuallyChecked() => ManuallyChecked.CheckedIntField;
+    public string GetIntManuallyChecked() => ManuallyChecked.CheckedStringField;
 
     [Benchmark]
-    public int GetIntChecked() => Checked.OwnedProperty;
+    public string GetIntChecked() => Checked.OwnedProperty;
 
 }
 
 public class PropertyOwner
 {
-    public int OwnedProperty { get; set; }
+    public string OwnedProperty { get; set; }
 }
 
-// public class ManuallyCheckedPropertyOwner
-// {
-//     public int OwnedProperty { get; set; }
-// }
-//
 public class CheckedPropertyOwner
 {
     [GetRequiresInit]
-    public int OwnedProperty { get; set; }
+    public string OwnedProperty { get; set; }
+}
+
+public class ManuallyChecked
+{
+    private bool _initFlag_CheckedIntField = false;
+    private string _checkedStringField;
+    public string CheckedStringField
+    {
+        get
+        {
+            if (!_initFlag_CheckedIntField) {
+                throw new InvalidOperationException();
+            }
+            return _checkedStringField;
+        }
+        set
+        {
+            _checkedStringField = value;
+            _initFlag_CheckedIntField = true;
+        }
+    }
 }
