3016fc2678f8a7dd20e3d42d6664bd68c706d575
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2022-04-23 22:02:17 -0400

    Fix smoke tests.

---
+++ SmokeTest/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace SmokeTestNamespace;
+
+using GetRequiresInit;
+
+// This file exists so that the GetRequiresInitAttribute shows up in the
+// Module's CustomAttributes. If it is missing, things break.
+
+[GetRequiresInit]
+class Class2
+{
+    public int Class2IntField { get; set; }
+}
+
+class Class3
+{
+    [GetRequiresInit]
+    public int Class3IntField { get; set; }
+}
