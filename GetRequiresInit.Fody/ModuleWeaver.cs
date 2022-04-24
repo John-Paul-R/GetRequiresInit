48bcf5db48438697515ba7c796829b7e0b0c6a52
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2022-04-23 21:38:38 -0400

    Distinct `propsToProcess`.

--- GetRequiresInit.Fody/ModuleWeaver.cs
+++ GetRequiresInit.Fody/ModuleWeaver.cs
@@ -33,25 +33,17 @@ public class ModuleWeaver :
         var typesToProcess = ModuleDefinition.Types
             .Where(t => t.HasCustomAttributes && t.CustomAttributes.Any(AttributeSelector))
             .ToList();
-        
-        var propsToProcess = typesToProcess
-            .SelectMany(t => t.Properties)
-            .ToList();
+
+        var propsFromAnnotatedTypes = typesToProcess
+            .SelectMany(t => t.Properties);
 
         var annotatedProps = ModuleDefinition.Types
             .SelectMany(t => t.Properties)
             .Where(p => p.HasCustomAttributes && p.CustomAttributes.Any(AttributeSelector));
-        
-        propsToProcess.AddRange(annotatedProps);
 
-        // TODO: Implement or remove `removeAttributes` config option
-        bool removeAttributes = false;
-        if (removeAttributes) {
-            foreach (var typeToProcess in typesToProcess) {
-                typeToProcess.CustomAttributes.Remove(typeToProcess.CustomAttributes.Single(AttributeSelector));
-            }
-        }
-
+        var propsToProcess = propsFromAnnotatedTypes
+            .Concat(annotatedProps)
+            .Distinct();
         foreach (var prop in propsToProcess) {
             ProcessProperty(prop);
         }
